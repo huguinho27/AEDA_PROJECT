063f458785a03d31458b6635276a07e49092d9f8
huguinho27 <up201404587@fe.up.pt>
2016-10-20 17:57:07 +0100

    Some developments on project.

---
+++ curricularUnit.h
@@ -0,0 +1,14 @@
+#ifndef CURRICULARUNIT_H
+#define CURRICULARUNIT_H
+#include <iostream>
+using namespace std;
+
+class curricularUnit
+{
+	string name;
+	vector<Student> students;
+	vector<Enunciation> enunciations;
+};
+
+
+#endif /* CURRICULARUNIT_H */
---
+++ general.cpp
@@ -0,0 +1,20 @@
+#include "curricularUnit.h"
+#include "enunciation.h"
+#include "student.h"
+#include <iostream>
+using namespace std;
+
+class general
+{
+	vector<curricularUnit> curricularUnits;
+	vector<Enunciation> enunciations;
+	vector<Student> students;
+public:
+
+
+};
+int main()
+{
+
+	return 0;
+}
