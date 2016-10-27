0c4986dbfe91f81e79f1fcae9cf3c7ed2fab49a5
TiagoC97 <tiago79c@gmail.com>
2016-10-27 17:40:56 +0100

    Added some functions

--- enunciation.h
+++ enunciation.h
@@ -8,12 +8,22 @@ class Enunciation
 {
 protected:
 	string title, description;
+	vector <string> years;
+	vector <vector <Student>> studentsByYear;
 public:
 	Enunciation(string title, string description);
 	string getTitle() const;
 	string getDescription() const;
+	vector <string> getYears();
+	vector <vector <Student>> getStudentsByYear();
 	void setTittle(string newTitle);
-	void setDescription(string novaDescription);
+	void setDescription(string newDescription);
+	void addYear(string newYear);
+	void addStudents(vector <Student> students);
+	void addStudentsOfYear(string newYear, vector <Student> students);
 };
 
+
+
 #endif /* ENUNCIATION_H */
+
