11ed8418a4b4471e2f3ee844f83f904b5510a13c
TiagoC97 <tiago79c@gmail.com>
2016-10-27 17:41:40 +0100

    Implemented some functions

--- enunciation.cpp
+++ enunciation.cpp
@@ -2,28 +2,47 @@
 
 using namespace std;
 
-Enunciation::Enunciation(string title, string description)
-{
+Enunciation::Enunciation(string title, string description){
 	this->title = title;
 	this->description = description;
 }
 
-string Enunciation::getTitle() const
-{
+string Enunciation::getTitle() const{
 	return title;
 }
 
-string Enunciation::getDescription() const
-{
+string Enunciation::getDescription() const{
 	return description;
 }
 
-void Enunciation::setTittle(string newTitle)
-{
+
+vector <string> Enunciation::getYears(){
+	return years;
+}
+
+vector <vector <Student>> Enunciation::getStudentsByYear(){
+	return studentsByYear;
+}
+
+
+void Enunciation::setTittle(string newTitle){
 	title = newTitle;
 }
 
-void Enunciation::setDescription(string newDescription)
-{
+void Enunciation::setDescription(string newDescription){
 	description = newDescription;
 }
+
+void Enunciation::addYear(string newYear){
+	years.push_back(newYear);
+}
+
+void Enunciation::addStudents(vector <Student> students){
+	studentsByYear.push_back(students);
+}
+
+void Enunciation::addStudentsOfYear(string newYear, vector <Student> students){
+	addYear(newYear);
+	addStudents(students);
+}
+
