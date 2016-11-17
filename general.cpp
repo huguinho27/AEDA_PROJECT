495e1a8539dc81551ca8ef81d805db3aabe874d9
huguinho27 <up201404587@fe.up.pt>
2016-11-17 12:46:55 +0000

    intermediate

--- general.cpp
+++ general.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <unistd.h>
+#include <string>
 #include "student.h"
 #include "curricularUnit.h"
 
@@ -12,28 +14,118 @@ class general
 public:
 	general();
 	void MainMenu();
+	bool verifyGetline(int init, int end, string input);
+	void browseGroupProjectMenu();
+	void browseTechersStudentMenu();
+	void browseEnunciationMenu();
+	void browseCurricularMenu();
 };
 
-general::general(){};
+general::general()
+{
+}
+;
+
+/**
+ * Checks if an input is a number and if it it between init and end values
+ * @return true if ok, false if not
+ */
+bool general::verifyGetline(int init, int end, string input)
+{
+	bool goodInput = false;
+
+	if (!goodInput)
+	{
+		bool number = true;
+
+		for (unsigned int i = 0; i < input.length() && number; i++)
+			if (!(input[i] >= '0' && input[i] <= '9'))
+				number = false;
+
+		if (number)
+		{
+			stringstream value_ss(input);
+			int value;
+			value_ss >> value;
+
+			goodInput = (value >= init && value <= end);
+		}
+	}
+	return goodInput;
+}
+
+void general::browseGroupProjectMenu()
+{
+	return;
+}
+
+void general::browseTechersStudentMenu()
+{
+	return;
+}
+
+void general::browseEnunciationMenu()
+{
+	return;
+}
+
+void general::browseCurricularMenu()
+{
+	system("cls");
+	string input;
+
+	cout << "CURRICULAR UNITS MANAGEMENT\n\n";
+	cout << "1. List Students of a Curricular Unit\n";
+	cout << "2. List Enunciations of this Curricular Unit\n";
+	cout << "3. Show Lecture Teacher\n";
+	cout << "4. Show Group Projects\n";
+	cout << "5. Back to Main Menu\n";
+
+	do
+	{
+		cout << ">> ";
+		getline(cin, input);
+		cin.clear();
+	} while (!verifyGetline(1, 5, input));
+
+	if (input == "1")
+		listStudentTeacher();
+	else if (input == "2")
+		listEnunciations();
+
+
+	return;
+}
 
 void general::MainMenu()
 {
-	int option;
+	system("cls");
+	string input;
+
 	cout << "ENUNCIATIONS MANAGEMENT OF FEUP\n\n";
 	cout << "1. Browse Curricular Units\n";
 	cout << "2. Browse Enunciations\n";
-	cout << "3. Browse Teachers/Students\n";
+	cout << "3. Browse Teachers / Students\n";
 	cout << "4. Browse Group Projects\n";
-	cout << "5. Exit";
-	cin.clear();
-	cin >> option;
+	cout << "5. Exit Program\n";
 
-	switch (option)
+	do
 	{
-	case 1:
-		//cout << "2" << endl;
-		break;
-	}
+		cout << ">> ";
+		getline(cin, input);
+		cin.clear();
+	} while (!verifyGetline(1, 5, input));
+
+	if (input == "1")
+		browseCurricularMenu();
+	else if (input == "2")
+		browseEnunciationMenu();
+	else if (input == "3")
+		browseTechersStudentMenu();
+	else if (input == "4")
+		browseGroupProjectMenu();
+	else if (input == "5")
+		exit(0);
 }
 
 int main()
