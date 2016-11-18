11f37276eb2fadccc45c149329c4fa69b774281c
Ilona26Generalova <ilona26generalova@gmail.com>
2016-11-18 13:42:19 +0000

    Add files via upload

--- groupProject.cpp
+++ groupProject.cpp
@@ -2,22 +2,28 @@
 
 groupProject::groupProject(){};
 
-groupProject::groupProject(vector<Student> studentsIN, Person teacherIN, string tFile)
+groupProject::groupProject(vector<Student> studentsIN, Professor teacherIN, string tFile, unsigned int num)
 {
 	students = studentsIN;
 	teacher = teacherIN;
 	textFile = tFile;
+	maxN = num;
 }
 
-bool groupProject::addStudent(Student s)
+bool groupProject::addStudent(Student st)
 {
-	if (students.size() < 3)
+	if (students.size() < maxN)
 	{
-		students.push_back(s);
+		students.push_back(st);
 		return true;
 	}
 	return false;
 }
+
+/*void groupProject::removeStudent(Student st)
+{
+
+}*/
 
 vector<Student> groupProject::getStudents() const
 {
--- groupProject.h
+++ groupProject.h
@@ -10,15 +10,23 @@ using namespace std;
 
 class groupProject
 {
-	Person teacher;
+	Professor teacher;
 	vector<Student> students;
 	string textFile;
+	unsigned int maxN;
 public:
 	groupProject();
-	groupProject(vector<Student> studentsIN, Person techerIN, string tFile);
-	string getTextFile();
-	bool addStudent(Student s);
+	groupProject(vector<Student> studentsIN, Professor teacherIN, string tFile, unsigned num);
+	bool addStudent(Student st);
+	void removeSudent(Student st);
+	void setTeacher(Professor prof);
+	void setTextFile(string tFile);
+	void setMark(Student st, double mark);
+	void setMaxNum(unsigned int num);
 	vector<Student> getStudents() const;
+	string getTextFile();
+	Professor getTeacher() const;
+	unsigned int getNum() const;
 };
 
 #endif
